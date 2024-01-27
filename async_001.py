510a5b9419870407bdee26568a3eeed06609e104
andre <andregarciacarneiro+bang@gmail.com>
2024-01-26 21:23:28 -0300

    - [MOD] async_001.py - documentation improved

--- async_001.py
+++ async_001.py
@@ -8,22 +8,28 @@ from constants import DATETIME_FMT, FMT_STR, LOG_LEVEL, PROCESS_RUNNING_PER_TIME
 
 
 # Controls tasks per time
-sem = Semaphore(3)
+sem = Semaphore(PROCESS_RUNNING_PER_TIME)
 
 
 def format_logging():
+    """Configures the logging instance"""
     logging.basicConfig(format=FMT_STR, level=LOG_LEVEL, datefmt=DATETIME_FMT)
 
 
 def get_processes():
+    """Simulates a getter function which returns a list of process ids"""
     return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 async def execute_process(process_id):
+    """Executes processes limiting the amount per time using a semaphore"""
     format_logging()
+    # Limiting tasks amount by semaphore parameter using the
+    # available generator
     async with sem:
         logging.debug(f"Executing process{str(process_id)}")
         start = time.perf_counter()
+        # Simulating the process
         await asyncio.sleep(randint(1, 10))
         end = time.perf_counter()
         time_spend = end - start
@@ -31,6 +37,7 @@ async def execute_process(process_id):
 
 
 async def execute_all():
+    """Executes all process by gathering all tasks based on the processes list"""
     processes = get_processes()
     tasks = []
     [tasks.append(asyncio.create_task(execute_process(p))) for p in processes]
@@ -48,4 +55,5 @@ async def main():
 
 
 if __name__ == '__main__':
+    # Starting the main loop
     asyncio.run(main())
